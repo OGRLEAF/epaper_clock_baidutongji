0b8a77be506f235b609d757c122c7d04e366e79e
OGRLEAF <666@orgleaf.com>
2017-10-21 21:39:37 +0800

    仅显示统计数据

--- tongji.py
+++ tongji.py
@@ -15,14 +15,11 @@ screen = Screen('/dev/ttyAMA0')
 screen.connect()
 screen.handshake()
 
-#screen.load_pic()
-# time.sleep(5)
-
 screen.clear()
 screen.set_memory(MEM_FLASH)
 screen.set_rotation(ROTATION_NORMAL)
 
-def weather_fail(msg):
+def tongji_fail(msg):
     screen.text(10,170, msg)
     screen.update()
     screen.disconnect()
@@ -35,29 +32,35 @@ try:
     with open(tongji_data_file, 'r') as in_file:
         tdata = json.load(in_file)
 except IOError:
-    weather_fail(u'ERROR:无法加载统计数据!')
-#print tdata_uv['body']['data'][0]['result']['items'][1][0][1]
+    tongji_fail(u'ERROR:无法加载统计数据!')
+    
 tdata_uv = str(tdata['body']['data'][0]['result']['items'][1][0][1]).decode("utf-8")
 tdata_pv = str(tdata['body']['data'][0]['result']['items'][1][0][0]).decode("utf-8")
 
 ydata_pv = str(tdata['body']['data'][0]['result']['items'][1][1][0]).decode("utf-8")
 ydata_uv = str(tdata['body']['data'][0]['result']['items'][1][1][1]).decode("utf-8")
-#print type(tdata_uv_unicode)
+
 
 screen.set_ch_font_size(FONT_SIZE_32)
 screen.set_en_font_size(FONT_SIZE_32)
 
-screen.text(590,130,u'今日PV')
-screen.text(700,130,tdata_pv)
+#文本显示位置
+tongji_x = 540
+tongji_y = 270
 
-screen.text(654,170,u'UV')
-screen.text(700,170,tdata_uv)
+screen.text(tongji_x,tongji_y - 42,u'-博客访问统计-')
+screen.text(tongji_x,tongji_y,u'今日PV')
+screen.text(tongji_x + 100,tongji_y,tdata_pv)
 
-screen.text(590,212,u'昨日PV')
-screen.text(700,212,ydata_pv)
+screen.text(tongji_x + 100 + 63,tongji_y,u'UV')
+screen.text(tongji_x + 100 + 100,tongji_y,tdata_uv)
 
-screen.text(654,254,u'UV')
-screen.text(700,254,ydata_uv)
+screen.text(tongji_x,tongji_y + 42,u'昨日PV')
+screen.text(tongji_x + 100,tongji_y + 42,ydata_pv)
+
+screen.text(tongji_x + 100 + 63,tongji_y + 42,u'UV')
+screen.text(tongji_x + 100 +100,tongji_y + 42,ydata_uv)
+
 screen.update()
 screen.disconnect()
 sys.exit(1)
